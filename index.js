f2d04987ada1a41e54f015e2ed02b05a87ba240c
protap1100 <protapb23@gmail.com>
2024-07-16 01:39:15 +0600

    add:approval added

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const express = require("express");
 const app = express();
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const cors = require("cors");
+const bcrypt = require("bcrypt");
 const port = process.env.PORT || 5000;
 
 // MiddleWare
@@ -31,14 +32,72 @@ async function run() {
     );
 
     // Define the collections
-    const database = client.db("Village-Management");
-    const registerCollection = database.collection("Projects");
+    const database = client.db("Job-Task");
+    const registerCollection = database.collection("userCollections");
 
+    app.post("/users", async (req, res) => {
+      const userData = req.body;
+      const hashedPassword = await bcrypt.hash(userData.password, 10);
+      userData.password = hashedPassword;
+      const result = await registerCollection.insertOne(userData);
+      res.send(result);
+    });
 
+    app.post("/login", async (req, res) => {
+      const { identifier, password } = req.body;
+      const user = await registerCollection.findOne({
+        $or: [{ email: identifier }, { number: identifier }],
+      });
+      if (!user) {
+        return res.status(401).json({ message: "Invalid login credentials" });
+      }
 
+      const isValidPassword = await bcrypt.compare(password, user.password);
+      if (!isValidPassword) {
+        return res.status(401).json({ message: "Invalid login credentials" });
+      }
 
+      res.json({ user });
+    });
 
+    app.get("/users", async (req, res) => {
+      const result = await registerCollection.find().toArray();
+      res.send(result);
+    });
 
+    app.put("/users/:id", async (req, res) => {
+      const { id } = req.params;
+      const { status } = req.body;
+
+      try {
+        const query = { _id: new ObjectId(id) };
+        const updateDocument = {
+          $set: {
+            status: status,
+          },
+        };
+        const result = await registerCollection.updateOne(
+          query,
+          updateDocument
+        );
+        if (result.matchedCount === 0) {
+          return res.status(404).json({ message: "User not found" });
+        }
+        res.json({ message: "User status updated to verified" });
+      } catch (error) {
+        res.status(500).json({
+          message: "An error occurred while updating the user status",
+        });
+      }
+    });
+
+    
+    app.delete("/users/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await registerCollection.deleteOne(query);
+      res.send(result);
+    });
   } catch (error) {
     console.error("Error connecting to MongoDB:", error);
   }
